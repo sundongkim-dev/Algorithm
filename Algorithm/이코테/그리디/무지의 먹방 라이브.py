65f989cf63c1bae2f35454afe5dd95c61c39f6b3
김선동 <mok03127@hanyang.ac.kr>
2022-10-19 16:55:03 +0900

    updates

---
+++ Algorithm/이코테/그리디/무지의 먹방 라이브.py
@@ -0,0 +1,14 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/42891?language=python3
+
+풀이 시작 시간: 2022-10-17 1
+풀이 종료 시간: 2022-10-17 1
+
+시간 제한 1초
+메모리 제한 128MB
+
+'''
+
+'''
+풀이: 
+'''
