bd8e59fc50452eb8e322a2b96cd9139e504cc6d4
Paul_Kim <mok03127@naver.com>
2022-10-20 13:04:42 +0900

    Update 무지의 먹방 라이브.py

--- Algorithm/이코테/그리디/무지의 먹방 라이브.py
+++ Algorithm/이코테/그리디/무지의 먹방 라이브.py
@@ -12,3 +12,35 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42891?language=python3
 '''
 풀이: 
 '''
+import heapq
+
+def solution(food_times, k):
+    answer = -1
+    q = []
+    
+    foods = len(food_times)
+        
+    for idx in range(foods):
+        heapq.heappush(q, (food_times[idx], idx+1))
+    
+    length = len(q)
+    prev = 0
+
+    while q:
+        gap = (q[0][0] - prev) * length
+        if gap <= k:
+            k -= gap
+            length -= 1
+            prev, _ = heapq.heappop(q)
+        else:
+            idx = k % length
+            q.sort(key = lambda x : x[1])
+            answer = q[idx][1]
+            break
+
+    return answer
+
+food_times = [3,1,2]
+k = 5
+result = solution(food_times, k)
+print(result)
